ee0dac0479e9039c672b2340b86b11116fb12c8c
alumno <alumno@MacBook-Pro-de-Usuario.local>
2017-09-02 10:30:35 -0500

    Clase01

---
+++ Javascript/Clase01/01-tipos-datos.js
@@ -0,0 +1,41 @@
+/*
+	Categoría: Primitivos
+		- Cadenas, textos, string, text 
+		- Números, numbers, númerico
+		- Boleanos
+
+	Categoría: Complejos
+		- Arreglos, Array
+		- Objeto tipo JSON
+		- Objeto tipo Map
+		- Objeto tipo Set
+*/
+
+var nombre = 'Sergio'
+
+var apellidos = " Hidalgo Cáceres"
+
+var edad = 40
+var altura = 1.90
+var valorPI = 3.141678989
+var color = 0xFF0000
+
+var finJuego = false
+var activo = true
+
+console.log(nombre)
+console.log(apellidos)
+console.log(edad)
+console.log(altura)
+console.log(valorPI)
+console.log(color)
+console.log(finJuego)
+console.log(activo)
+
+console.log(nombre + apellidos)
+
+var edadPromedio = edad * 1.2
+console.log(edadPromedio)
+
+var edadCadena = '40'
+console.log(edadCadena + 10)
